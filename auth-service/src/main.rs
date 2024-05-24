ce6a5b691875d7425a92897c780b3bd57946aade
David C Black <david.black@doulos.com>
2024-05-24 16:11:17 -0500

    add user state

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -5,8 +5,8 @@ use tokio::sync::RwLock;
 
 #[tokio::main]
 async fn main() {
-    let user_store = Arc::new(RwLock::new(HashmapUserStore::default()));
-    let app_state = crate::app_state::AppState::new(user_store);
+    let user_store = Arc::new(RwLock::new(auth_service::services::HashmapUserStore::default()));
+    let app_state = auth_service::app_state::AppState::new(user_store);
     let app = Application::build( app_state, "0.0.0.0:3000")
         .await
         .expect("Failed to build app");
