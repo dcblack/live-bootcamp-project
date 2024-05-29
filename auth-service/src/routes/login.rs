73500295836a5365444bfd0139e08da013ab900b
David C Black <david.black@doulos.com>
2024-05-29 08:34:12 -0500

    Attempt to add more meaningful implementation - compiles.
    Signed-off-by: David C Black <david.black@doulos.com>

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,5 +1,50 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, Json, response::IntoResponse};
+use axum::extract::State;
+use serde::{Deserialize, Serialize};
+use crate::{
+    app_state::AppState,
+    domain::{AuthAPIError, Email, Password},
+};
 
-pub async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+pub async fn login(
+    State(state): State<AppState>,
+    Json(request): Json<LoginRequest>,
+) -> Result<impl IntoResponse, crate::domain::error::AuthAPIError > {
+    let email =
+      Email::parse(request.email.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+    let password =
+      Password::parse(request.password.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+    let user_store = state.user_store.read().await;
+    match user_store.get_user(&email).await {
+        Err(_) => { return Err(AuthAPIError::InvalidCredentials); },
+        Ok(user) => {
+            // Check request against the password
+            if user.password == password {
+            if user.requires_2fa {
+                let response = Json(LoginResponse {
+                    message: "Login successful!".to_string(),
+                });
+                return Ok((StatusCode::ACCEPTED, response));
+            } else {
+                let response = Json(LoginResponse {
+                    message: "Login successful!".to_string(),
+                });
+                return Ok((StatusCode::ACCEPTED, response));
+            }
+          } else {
+            return Err(AuthAPIError::InvalidCredentials);
+          }
+        }
+    }
+}
+
+#[derive(Deserialize,Clone)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct LoginResponse {
+    pub message: String,
 }
